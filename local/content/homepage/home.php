aec1728973e9ad7f045836b84d9202435823a8d9
Matt Reishman <matt.reishman@me.com>
2018-11-04 14:52:52 -0600

    small changes

--- local/content/homepage/home.php
+++ local/content/homepage/home.php
@@ -26,10 +26,6 @@
 </div>
 
 <style type="text/css">
-	body, html {
-	    height: 100%;
-	}
-
 	.category-buttons{
 		height: 200px;
 		width: 350px;
@@ -37,28 +33,21 @@
 		margin: 2%;
 	}
 
-	/* The hero image */
 	.hero-image {
-	    /* Use "linear-gradient" to add a darken background effect to the image (photographer.jpg). This will make the text easier to read */
-	    background-image: linear-gradient(rgba(25, 25, 25, 0.5), rgba(0, 0, 0, 0.5)), url("media/test.png");
-
-	    /* Set a specific height */
-	    height: 50%;
-
-	    /* Position and center the image to scale nicely on all screens */
-	    background-position: center;
-	    background-repeat: no-repeat;
-	    background-size: cover;
-	    position: relative;
+		background-image: linear-gradient(rgba(25, 25, 25, 0.5), rgba(0, 0, 0, 0.5)), url("media/img/home/main_banner.jpg");
+		height: 50%;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
 	}
 
-	/* Place text in the middle of the image */
 	.hero-text {
-	    text-align: center;
-	    position: absolute;
-	    top: 50%;
-	    left: 50%;
-	    transform: translate(-50%, -50%);
-	    color: white;
+		text-align: center;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
 	}
 </style>

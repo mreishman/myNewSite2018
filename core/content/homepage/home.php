509d2552be5e4e68f9486794adfaff60598f038f
Matt Reishman <matt.reishman@me.com>
2018-11-10 15:33:11 -0600

    Updated core to version 0.1

--- core/content/homepage/home.php
+++ core/content/homepage/home.php
@@ -2,7 +2,7 @@
 	<h1 style="text-align: center;" >
 		WebCoreMJR 0.1
 	</h1>
-	<p>
-		Some brief information about WebCoreMJR.
+	<p style="text-align: center;">
+		WebCoreMJR is a somewhat simple website framework that uses php and xml, and doesn't use a databse. I mainly created this for use in my own websites, but decided to create a dedicated website for it.
 	</p>
 </div>

0dd6c65ea46d1cf5533c3d773487f667e8fcf60d
Matt Reishman <matt.reishman@me.com>
2018-11-22 17:45:46 -0600

    Updated with 0.1.0.1 of WCMJR

--- core/content/templates/oneColumn.php
+++ core/content/templates/oneColumn.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php echo $baseXmlGen->title; ?></title>
 		<link rel="shortcut icon" type="image/png" href="/media/img/main/favicon.png"/>
 	</head>

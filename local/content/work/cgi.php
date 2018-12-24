d7fc66c06bf26c319d172c49d644e63c253d794b
Matt Reishman <matt.reishman@me.com>
2018-12-24 09:40:36 -0600

    added cgi galleries

---
+++ local/content/work/cgi.php
@@ -0,0 +1,251 @@
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2016</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="230px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2016SnowGlobe",
+						"thumb"	=>	"/media/img/work/images/Snowglobe2016_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Snowglobe2016.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2015</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="1100px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2015Forest",
+						"thumb"	=>	"/media/img/work/images/Woods_Fire_Final_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Woods_Fire_Final.jpg"
+					),
+					1	=> array(
+						"id"	=>	"2015D6",
+						"thumb"	=>	"/media/img/work/images/Decay_6_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Decay_6.jpg"
+					),
+					2	=> array(
+						"id"	=>	"2015D5",
+						"thumb"	=>	"/media/img/work/images/Decay_5_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Decay_5.jpg"
+					),
+					3	=> array(
+						"id"	=>	"2015D2",
+						"thumb"	=>	"/media/img/work/images/Decay_2_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Decay_2.jpg"
+					),
+					4	=> array(
+						"id"	=>	"2015D1",
+						"thumb"	=>	"/media/img/work/images/Decay_1_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Decay_1.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2014</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="880px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2014Time",
+						"thumb"	=>	"/media/img/work/images/Time_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Time.jpg"
+					),
+					1	=> array(
+						"id"	=>	"2014Gem",
+						"thumb"	=>	"/media/img/work/images/Gem_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Gem.jpg"
+					),
+					2	=> array(
+						"id"	=>	"2014Lift",
+						"thumb"	=>	"/media/img/work/images/Lift_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Lift.jpg"
+					),
+					3	=> array(
+						"id"	=>	"2014Clocks",
+						"thumb"	=>	"/media/img/work/images/Clocks_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Clocks.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2013</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="1320px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2013AAC",
+						"thumb"	=>	"/media/img/work/images/AAC_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/AAC.jpg"
+					),
+					1	=> array(
+						"id"	=>	"2013AAC2",
+						"thumb"	=>	"/media/img/work/images/AAC2_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/AAC2.jpg"
+					),
+					2	=> array(
+						"id"	=>	"2013AAC3",
+						"thumb"	=>	"/media/img/work/images/AAC3_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/AAC3.jpg"
+					),
+					3	=> array(
+						"id"	=>	"2013Violin",
+						"thumb"	=>	"/media/img/work/images/Violin_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Violin.jpg"
+					),
+					4	=> array(
+						"id"	=>	"2013Change",
+						"thumb"	=>	"/media/img/work/images/Change_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Change.jpg"
+					),
+					5	=> array(
+						"id"	=>	"20132035",
+						"thumb"	=>	"/media/img/work/images/2035_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/2035.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2012</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="1320px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2012Piano",
+						"thumb"	=>	"/media/img/work/images/Piano_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Piano.jpg"
+					),
+					1	=> array(
+						"id"	=>	"2012DesertHeart",
+						"thumb"	=>	"/media/img/work/images/DesertHeart_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/DesertHeart.jpg"
+					),
+					2	=> array(
+						"id"	=>	"2012Boat",
+						"thumb"	=>	"/media/img/work/images/Boat_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Boat.jpg"
+					),
+					3	=> array(
+						"id"	=>	"2012Sparks",
+						"thumb"	=>	"/media/img/work/images/Sparks_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Sparks.jpg"
+					),
+					4	=> array(
+						"id"	=>	"2012Splash",
+						"thumb"	=>	"/media/img/work/images/Splash_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Splash.jpg"
+					),
+					5	=> array(
+						"id"	=>	"2012Domino",
+						"thumb"	=>	"/media/img/work/images/Domino_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Domino.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>2011</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="660"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"id"	=>	"2011RC",
+						"thumb"	=>	"/media/img/work/images/RC_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/RC.jpg"
+					),
+					1	=> array(
+						"id"	=>	"2011IDK",
+						"thumb"	=>	"/media/img/work/images/IDK_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/IDK.jpg"
+					),
+					2	=> array(
+						"id"	=>	"2011Lamp",
+						"thumb"	=>	"/media/img/work/images/Lamp_Thumb.jpg",
+						"src"	=>	"/media/img/work/images/Lamp.jpg"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>

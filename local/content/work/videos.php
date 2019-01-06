dccb3ec46261e0a91b2d78ddf04ef9439c4767e6
Matt Reishman <matt.reishman@me.com>
2019-01-06 10:25:54 -0600

    Video page updated with new gallery logic

---
+++ local/content/work/videos.php
@@ -0,0 +1,185 @@
+<table width="100%">
+	<tr>
+		<td>
+			<h2>Featured</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="660px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"link"		=>	"https://youtu.be/wSHOeGtGTZg",
+						"src"		=>	"/media/img/work/videoThumb/NLC_2016_PBL.jpg",
+						"target"	=>	"_blank"
+					),
+					1	=> array(
+						"link"		=>	"https://youtu.be/PRTD5HnVgVw",
+						"src"		=>	"/media/img/work/videoThumb/ArchAcadComp.jpg",
+						"target"	=>	"_blank"
+					),
+					2	=> array(
+						"link"		=>	"https://youtu.be/4D-_m4QUvGM",
+						"src"		=>	"/media/img/work/videoThumb/FBLA_Middle_Level_SLC_2015.jpg",
+						"target"	=>	"_blank"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>FBLA</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="660px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"link"		=>	"https://youtu.be/4D-_m4QUvGM",
+						"src"		=>	"/media/img/work/videoThumb/FBLA_Middle_Level_SLC_2015.jpg",
+						"target"	=>	"_blank"
+					),
+					1	=> array(
+						"link"		=>	"https://youtu.be/qN6WBb24qiw",
+						"src"		=>	"/media/img/work/videoThumb/SLC_2016_Preview.jpg",
+						"target"	=>	"_blank"
+					),
+					2	=> array(
+						"link"		=>	"https://youtu.be/bAPeyqiTXBI",
+						"src"		=>	"/media/img/work/videoThumb/FBLA-Friends.jpg",
+						"target"	=>	"_blank"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>My Channel</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="880px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"link"		=>	"https://youtu.be/PRTD5HnVgVw",
+						"src"		=>	"/media/img/work/videoThumb/ArchAcadComp.jpg",
+						"target"	=>	"_blank"
+					),
+					1	=> array(
+						"link"		=>	"https://youtu.be/xYhgM_3vwSA",
+						"src"		=>	"/media/img/work/videoThumb/Blueprint_Moto.jpg",
+						"target"	=>	"_blank"
+					),
+					2	=> array(
+						"link"		=>	"https://youtu.be/hTXnctuESf0",
+						"src"		=>	"/media/img/work/videoThumb/Decay-1.jpg",
+						"target"	=>	"_blank"
+					),
+					3	=> array(
+						"link"		=>	"https://youtu.be/FZpoZ980mJY",
+						"src"		=>	"/media/img/work/videoThumb/Linc_Timelapse_Thumb.jpg",
+						"target"	=>	"_blank"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>PBL</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="880px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"link"		=>	"https://youtu.be/wSHOeGtGTZg",
+						"src"		=>	"/media/img/work/videoThumb/NLC_2016_PBL.jpg",
+						"target"	=>	"_blank"
+					),
+					1	=> array(
+						"link"		=>	"https://youtu.be/zCPGMXDEARM",
+						"src"		=>	"/media/img/work/videoThumb/2015_2016_PBL_Recap.jpg",
+						"target"	=>	"_blank"
+					),
+					2	=> array(
+						"link"		=>	"https://youtu.be/uq1hTZWfnM4",
+						"src"		=>	"/media/img/work/videoThumb/NFLC2015_Thumbnail.jpg",
+						"target"	=>	"_blank"
+					),
+					3	=> array(
+						"link"		=>	"https://youtu.be/yuDKf4Ss9-s",
+						"src"		=>	"/media/img/work/videoThumb/ShowmeConference2015_Thumbnail.jpg",
+						"target"	=>	"_blank"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>
+<table width="100%">
+	<tr>
+		<td>
+			<h2>Anime Party</h2>
+		</td>
+	</tr>
+</table>
+<div style="overflow:auto;">
+	<table width="440px"  align="left">
+		<tr>
+			<?php echo $gallery->generateGallery(
+				array(
+					0	=> array(
+						"link"		=>	"https://youtu.be/UPUSi1ORwIQ",
+						"src"		=>	"/media/img/work/videoThumb/Halloween_Party_2015_Thumbnail.jpg",
+						"target"	=>	"_blank"
+					),
+					1	=> array(
+						"link"		=>	"https://youtu.be/wXwfqnDX29g",
+						"src"		=>	"/media/img/work/videoThumb/Halloween-Timelapse-Thumbnail.jpg",
+						"target"	=>	"_blank"
+					)
+				),
+				array(
+					"imgHeight"	=>	"157",
+					"tag"		=>	"th"
+				)
+			); ?>
+		</tr>
+	</table>
+</div>

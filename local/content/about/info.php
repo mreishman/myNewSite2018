bf1f201a88179c24344daa27ad69bee28195191e
Matt Reishman <matt.reishman@me.com>
2019-01-12 09:45:00 -0600

    Updated footer

--- local/content/about/info.php
+++ local/content/about/info.php
@@ -146,11 +146,11 @@
                 "Bug Fixes"      =>  array("Updated WebCoreMJR to 0.1.2.1","Updated some gallery images")
             )
         ),
-        19  =>  array(
+        20  =>  array(
             "version"       =>  "3.0.2.2",
             "date"          =>  "1/12/2019",
             "notes"         =>  array(
-                "Bug Fixes"      =>  array("General Content Update")
+                "Content Update"      =>  array("Added link to WebCoreMJR","Added changelog link in footer","Updated social media links","Added old 2014 portfolio to gallery")
             )
         )
       );

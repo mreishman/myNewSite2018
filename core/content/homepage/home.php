95179890b07a0741148a064edb5f1ea071322400
Matt Reishman <matt.reishman@me.com>
2019-01-11 22:00:25 -0600

    Removed webcore demo site content

--- core/content/homepage/home.php
+++
@@ -1,8 +0,0 @@
-<div>
-	<h1 style="text-align: center;" >
-		WebCoreMJR 0.1.2
-	</h1>
-	<p style="text-align: center;">
-		WebCoreMJR is a somewhat simple website framework that uses php and xml, and doesn't use a database. I mainly created this for use in my own websites, but decided to create a dedicated website for it.
-	</p>
-</div>
